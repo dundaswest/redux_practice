684c06b5bc10e7c692730654d5c1b663a84df16e
Eunbyul Cho <hello@eunbyulcho.me>
2019-06-08 03:30:23 +0900

    fix webpack

--- learn-redux/webpack.config.dev.js
+++ learn-redux/webpack.config.dev.js
@@ -3,10 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   devtool: 'source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './client/reduxstagram'
-  ],
+  entry: ['webpack-hot-middleware/client', './client/reduxstagram'],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -14,22 +11,23 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin()
   ],
   module: {
-    loaders: [
-    // js
-    {
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'client')
-    },
-    // CSS
-    { 
-      test: /\.styl$/, 
-      include: path.join(__dirname, 'client'),
-      loader: 'style-loader!css-loader!stylus-loader'
-    }
+    rules: [
+      // js
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+
+        include: path.join(__dirname, 'client')
+      },
+      // CSS
+      {
+        test: /\.styl$/,
+        include: path.join(__dirname, 'client'),
+        loader: 'style-loader!css-loader!stylus-loader'
+      }
     ]
   }
 };
