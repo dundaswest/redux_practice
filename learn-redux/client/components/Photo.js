9b84b9d2a760192b3dcd7899840a61ef34b94420
Eunbyul Cho <hello@eunbyulcho.me>
2019-06-12 06:53:48 +0900

    add photo component and set like btn

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -1,7 +1,46 @@
 import React from 'react';
-
+import PhotoGrid from './PhotoGrid';
+import { Link } from 'react-router';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 const Photo = React.createClass({
   render() {
-    return <div>I am a photo</div>;
+    const { post, i, comments } = this.props;
+    return (
+      <figure className="grid-figure">
+        <div className="grid-photo-wrap">
+          <Link to={`/view/${post.code}`}>
+            <img
+              src={post.display_src}
+              alt={post.caption}
+              className="grid-photo"
+            />
+          </Link>
+          <CSSTransitionGroup
+            transitionName="like"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}
+          >
+            <span key={post.likes} className="likes-heart">
+              {post.likes}
+            </span>
+          </CSSTransitionGroup>
+        </div>
+        <figcaption>
+          <p>{post.caption}</p>
+          <div className="control-buttons">
+            <button className="likes">&hearts; {post.likes}</button>
+            <Link to={`/view/${post.code}`}>
+              <span className="comment-count">
+                <span className="speech-bubble">
+                  {comments[post.code] ? comments[post.code].length : 0}
+                </span>
+              </span>-
+            </Link>
+          </div>
+        </figcaption>
+      </figure>
+    );
   }
 });
+
+export default Photo;
--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,11 +1,13 @@
-import React from "react";
-
+import React from 'react';
+import Photo from './Photo';
 class PhotoGrid extends React.Component {
   render() {
     return (
       <div className="photo-grid">
         i am the singe
-        {JSON.stringify(this.props.posts, null, " ")}
+        {this.props.posts.map((post, i) => (
+          <Photo {...this.props} key={i} i={i} post={post} />
+        ))}
       </div>
     );
   }
