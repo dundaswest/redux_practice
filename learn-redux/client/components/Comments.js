d72ff91e1eea0a3975deebf586e2485149d8c400
Eunbyul Cho <hello@eunbyulcho.me>
2019-06-13 07:27:10 +0900

    setUp postComment reducer

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import comments from '../reducers/comments';
+
+const Comments = React.createClass({
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  },
+  handleSubmit(e) {
+    e.preventDefault(e);
+
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    console.log(postId, author, comment);
+    this.props.addComment(postId, author, comment);
+  },
+  render() {
+    return (
+      <div className="comment">
+        I'm the comments!!!
+        {this.props.postComments.map(this.renderComment)}
+        <form
+          ref="commentForm"
+          className="comment-form"
+          onSubmit={this.handleSubmit}
+        >
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
+      </div>
+    );
+  }
+});
+
+export default Comments;
